b501b81488dc6e2addc19ebbf151651c8d458cb8
anusha80563 <89307470+anusha80563@users.noreply.github.com>
2021-08-31 10:57:45 +0530

    Add files via upload

---
+++ E Commerce.py
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Products</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+      <a class="navbar-brand">ECOMMERCE</a>
+      <form class="form-inline">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign Up</button>
+      </form>
+    </nav>     
+    <div class="container">
+      <div class="card-group">
+	  <div class="card">
+	    <img class="card-img-top" src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1889&q=80" alt="card-img-cap">
+	    <div class="card-body">
+	      <h5 class="card-title">Android Smartwatch</h5>
+	      <h6> Category : Watch</h6>
+	      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>  
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>    
+            </div>
+               <button class="btn btn-primary">Buy Now</button>
+	  </div>
+	  <div class="card">
+	    <img class="card-img-top" src="https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" alt="card-img-cap">
+	    <div class="card-body">
+	      <h5 class="card-title"Sunglasses from Gentle Monster></h5>
+	      <h6>Category : Glass</h6>
+	      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+	      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+	    </div>
+	       <button class="btn btn-primary">Buy Now</button>       
+	  </div>
+	  <div class="card">
+	    <img class="card-img-top" src="https://images.unsplash.com/photo-1484704849700-f032a568e944?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80" alt="card-img-cap">
+	    <div class="card-body">
+	      <h5 class="card-title"Listening To Music></h5>
+	      <h6>Category : Music</h6>
+	      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+	      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
